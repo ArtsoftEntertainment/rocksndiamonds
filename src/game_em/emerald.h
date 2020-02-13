dc394ec0acce55b93d0a79bd7990127dbad3d389
Holger Schemel <info@artsoft.org>
2020-02-13 20:48:27 +0100

    changed order of some level structure values for EM engine

--- src/game_em/emerald.h
+++ src/game_em/emerald.h
@@ -632,44 +632,47 @@ struct LOGIC
   int required;			/* emeralds needed */
   int score;			/* score */
 
-  int alien_score;		/* score for killing alien */
-  int amoeba_time;		/* amoeba speed */
-  int android_move_cnt;		/* android move counter */
-  int android_move_time;	/* android move reset time */
-  int android_clone_cnt;	/* android clone counter */
-  int android_clone_time;	/* android clone reset time */
-  int ball_cnt;			/* ball counter */
-  int ball_pos;			/* ball array pos counter */
-  int ball_random;		/* ball is random flag */
-  int ball_state;		/* ball active flag */
-  int ball_time;		/* ball reset time */
-  int bug_score;		/* score for killing bug */
-  int diamond_score;		/* score for collecting diamond */
-  int dynamite_score;		/* score for collecting dynamite */
-  int eater_pos;		/* eater array pos */
   int eater_score;		/* score for killing eater */
-  int emerald_score;		/* score for collecting emerald */
-  int exit_score;		/* score for entering exit */
-  int key_score;		/* score for colleting key */
-  int lenses_cnt;		/* lenses counter */
-  int lenses_score;		/* score for collecting lenses */
-  int lenses_time;		/* lenses reset time */
-  int magnify_cnt;		/* magnify counter */
-  int magnify_score;		/* score for collecting magnifier */
-  int magnify_time;		/* magnify reset time */
-  int nut_score;		/* score for cracking nut */
-  int shine_cnt;		/* shine counter for emerald/diamond */
-  int slurp_score;		/* score for slurping alien */
+  int alien_score;		/* score for killing alien */
+  int bug_score;		/* score for killing bug */
   int tank_score;		/* score for killing tank */
-  int wheel_cnt;		/* wheel counter */
+  int emerald_score;		/* score for collecting emerald */
+  int diamond_score;		/* score for collecting diamond */
+  int nut_score;		/* score for cracking nut */
+  int slurp_score;		/* score for slurping alien with spring */
+  int dynamite_score;		/* score for collecting dynamite */
+  int key_score;		/* score for colleting key */
+  int lenses_score;		/* score for collecting lenses */
+  int magnify_score;		/* score for collecting magnifier */
+  int exit_score;		/* score for entering exit */
+
+  int android_move_time;	/* android move reset time */
+  int android_clone_time;	/* android clone reset time */
+  int ball_time;		/* ball reset time */
+  int amoeba_time;		/* amoeba speed */
+  int wonderwall_time;		/* wonderwall time */
+  int wheel_time;		/* wheel reset time */
   int wheel_x;			/* wheel x pos */
   int wheel_y;			/* wheel y pos */
-  int wheel_time;		/* wheel reset time */
-  int wind_cnt;			/* wind time counter */
-  int wind_direction;		/* wind direction */
+  int lenses_time;		/* lenses reset time */
+  int magnify_time;		/* magnify reset time */
   int wind_time;		/* wind reset time */
+  int wind_direction;		/* wind direction */
+
+  int ball_random;		/* ball is random flag */
+  int ball_state;		/* ball active flag */
   int wonderwall_state;		/* wonderwall active flag */
-  int wonderwall_time;		/* wonderwall time */
+  int wheel_cnt;		/* wheel counter */
+  int lenses_cnt;		/* lenses counter */
+  int magnify_cnt;		/* magnify counter */
+  int wind_cnt;			/* wind time counter */
+
+  int android_move_cnt;		/* android move counter */
+  int android_clone_cnt;	/* android clone counter */
+  int ball_cnt;			/* ball counter */
+  int ball_pos;			/* ball array pos counter */
+  int eater_pos;		/* eater array pos */
+  int shine_cnt;		/* shine counter for emerald/diamond */
 
   int num_ball_arrays;		/* number of ball data arrays used */
 
@@ -683,7 +686,7 @@ struct LOGIC
 
   short eater_array[8][9];		/* eater data */
   short ball_array[8][8];		/* ball data */
-  short android_array[TILE_MAX];	/* android clone table */
+  short android_array[TILE_MAX];	/* android clone data */
 
   short cavebuf[CAVE_BUFFER_WIDTH][CAVE_BUFFER_HEIGHT];
   short nextbuf[CAVE_BUFFER_WIDTH][CAVE_BUFFER_HEIGHT];
