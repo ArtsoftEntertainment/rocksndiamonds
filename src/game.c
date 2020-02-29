7f125847296380e85814a98acb6a0b0dab4c2cf6
Holger Schemel <info@artsoft.org>
2020-02-29 14:40:36 +0100

    fixed initializing locally connected player in single player mode

--- src/game.c
+++ src/game.c
@@ -3753,12 +3753,13 @@ void InitGame(void)
   // use preferred player also in local single-player mode
   if (!network.enabled && !game.team_mode)
   {
-    int old_index_nr = local_player->index_nr;
     int new_index_nr = setup.network_player_nr;
 
     if (new_index_nr >= 0 && new_index_nr < MAX_PLAYERS)
     {
-      stored_player[old_index_nr].connected_locally = FALSE;
+      for (i = 0; i < MAX_PLAYERS; i++)
+	stored_player[i].connected_locally = FALSE;
+
       stored_player[new_index_nr].connected_locally = TRUE;
     }
   }
