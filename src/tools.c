ArtsoftEntertainment/rocksndiamonds

commit 472e5e9019c2af26bd5f3dfd2651b1b47aadf7bb

fixed graphical bugs when using background graphics on menu screens
This bug occurs when using background graphics on menu screens in
combination with a bottom screen tape panel (without using the normal
request door, but using envelope style door requests instead). This
caused problems with black boxes in the bottom right corner on all
sub-screens (like info, setup or scores screens).

This bugfix now also allows non-black backgrounds for bottom screen
tape panels (by defining the main screen to use the whole window,
including the area reserved for the bottom screen tape panel, which
uses the main screen's background graphics during sliding in or out).

src/tools.c
@@ -5580,8 +5580,8 @@ unsigned int MoveDoor(unsigned int door_state)
 
 	if (!door_panel_drawn[door_index])
 	{
-	  ClearRectangle(drawto, door_rect->x, door_rect->y,
-			 door_rect->width, door_rect->height);
+	  ClearRectangleOnBackground(drawto, door_rect->x, door_rect->y,
+				     door_rect->width, door_rect->height);
 
 	  door_panel_drawn[door_index] = TRUE;
 	}

commit 5a51d44ed3703e19c6f410f83a3d055b2ac88969

fixed graphical bug with door background during request dialogs
This fixes a regression caused by commit 472e5e90.

src/tools.c
@@ -5515,6 +5515,8 @@ unsigned int MoveDoor(unsigned int door_state)
       }
     }
 
+    SetDoorBackgroundImage(IMG_BACKGROUND_DOOR);
+
     for (k = start; k < num_move_steps; k++)
     {
       int last_frame = num_move_steps - 1;	// last frame of this "for" loop