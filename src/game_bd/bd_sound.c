ArtsoftEntertainment/rocksndiamonds

commit 386e26cf0f543f4c49d2231cacf79b5bcfc5bb61

fixed playing sounds at wrap-around BD cave positions
This change completes commit daf84901, which only fixed sounds at
wrap-around BD cave positions when using the non-native sound engine.

src/game_bd/bd_sound.c
@@ -478,6 +478,10 @@ static void gd_sound_info_to_play(int channel, int x, int y, int element, int so
 // plays sound in a cave
 void gd_sound_play(GdCave *cave, GdSound sound, GdElement element, int x, int y)
 {
+  // fix wrap-around cave positions
+  x = (x + cave->w) % cave->w;
+  y = (y + cave->h) % cave->h;
+
   if (sound == GD_S_NONE)
     return;
 
@@ -495,10 +499,6 @@ void gd_sound_play(GdCave *cave, GdSound sound, GdElement element, int x, int y)
 
   if (!game.use_native_bd_sound_engine)
   {
-    // fix wrap-around cave positions for non-native sound engine
-    x = (x + cave->w) % cave->w;
-    y = (y + cave->h) % cave->h;
-
     // when not using native sound engine, just play the sound
     PlayLevelSound_BD(x, y, element, sound);
 

commit 4c8fe1a3afdbe4c3188db6ebe23b768027d59456

fixed playing loop sounds when using native BD game engine
When using the native BD game engine, but not using the native BD
sound engine, loop sounds were not played anymore due to a bug with
off-playfield sound positions.

This bug was introduced by commit 386e26cf.

src/game_bd/bd_sound.c
@@ -478,9 +478,12 @@ static void gd_sound_info_to_play(int channel, int x, int y, int element, int so
 // plays sound in a cave
 void gd_sound_play(GdCave *cave, GdSound sound, GdElement element, int x, int y)
 {
-  // fix wrap-around cave positions
-  x = (x + cave->w) % cave->w;
-  y = (y + cave->h) % cave->h;
+  if (x != -1 || y != -1)
+  {
+    // fix wrap-around cave positions
+    x = (x + cave->w) % cave->w;
+    y = (y + cave->h) % cave->h;
+  }
 
   if (sound == GD_S_NONE)
     return;