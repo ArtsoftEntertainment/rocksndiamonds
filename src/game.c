ArtsoftEntertainment/rocksndiamonds

commit feec08d456b68110fb8c9b3d38b3f21cebe065fe

fixed bug with closing tape recorder door after game
Only close tape recorder door when entering hall of fame, but not when
playing next game or when returning to main menu.

src/game.c
@@ -5356,9 +5356,6 @@ void GameEnd(void)
     strcpy(tape.score_tape_basename, getScoreTapeBasename(setup.player_name));
   }
 
-  // if no tape is to be saved, close both doors simultaneously
-  CloseDoor(DOOR_CLOSE_ALL);
-
   if (level_editor_test_game || score_info_tape_play)
   {
     SetGameStatus(GAME_MODE_MAIN);
@@ -5393,6 +5390,8 @@ void GameEnd(void)
 
   if (game.LevelSolved_SaveScore && scores.last_added >= 0 && setup.show_scores_after_game)
   {
+    CloseDoor(DOOR_CLOSE_ALL);
+
     SetGameStatus(GAME_MODE_SCORES);
 
     DrawHallOfFame(last_level_nr);

commit fdb5c6f43ba850804e681002f9bfdc7584b42513

fixed showing wrong playfield actions when closing "replace tape" door
This bug was introduced by commit feec08d4.

src/game.c
@@ -5393,9 +5393,9 @@ void GameEnd(void)
 
   if (game.LevelSolved_SaveScore && scores.last_added >= 0 && setup.show_scores_after_game)
   {
-    CloseDoor(DOOR_CLOSE_ALL);
-
     SetGameStatus(GAME_MODE_SCORES);
+
+    CloseDoor(DOOR_CLOSE_ALL);
 
     DrawHallOfFame(last_level_nr);
   }