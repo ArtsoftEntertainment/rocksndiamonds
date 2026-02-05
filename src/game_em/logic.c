ArtsoftEntertainment/rocksndiamonds

commit ab75a6b93e4f63fcecb4ff8eb4437a487ac2d40f

improved amoeba growth speed for native EM game engine even more
This change improves the accuracy of higher and lower amoeba speeds
even more (and is the result of amoeba growth tests playing various
levels that were compared side by side with an Amiga emulator with
the same test levels). This change especially improves the accuracy
of very low amoeba speeds, which were growing just too slow before.

src/game_em/logic.c
@@ -7918,7 +7918,7 @@ static void logic_globals(void)
   }
   else
   {
-    int amoeba_count = MIN(lev.amoeba_time * 8, 999);
+    int amoeba_count = MIN(lev.amoeba_time * 8 + 4, 999);
 
     seed = game_em.random;
 

commit 6c94b97ed594f8c1e7e0866308401e75cd04ca39

fixed bug with dead, but growing amoeba for native EM game engine
This fixes EM style amoeba that was growing despite having a growth
speed of "0".

This bug was introduced by commit ab75a6b9.

src/game_em/logic.c
@@ -7932,7 +7932,7 @@ static void logic_globals(void)
 
     game_em.random = amoeba_random;
   }
-  else
+  else if (lev.amoeba_time > 0)
   {
     int amoeba_count = MIN(lev.amoeba_time * 8 + 4, 999);
 