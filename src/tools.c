ArtsoftEntertainment/rocksndiamonds

commit 06aebcbcc76e960f85a09222369276032340020c

fixed checking text position relative to viewport

src/tools.c
@@ -3595,7 +3595,10 @@ static int getMaxTextLength(struct TextPosInfo *pos, int font_nr)
 
 static boolean inGfxField_FontHeight(struct TextPosInfo *pos)
 {
-  if (!IN_GFX_FIELD_FULL(pos->x, pos->y + getFontHeight(pos->font)))
+  if (pos->x == -1 && pos->y == -1)	// marked as outside viewport
+    return FALSE;
+
+  if (!IN_GFX_FIELD_FULL(SX + pos->x, SY + pos->y + getFontHeight(pos->font)))
     return FALSE;
 
   return TRUE;

commit afeb3ae2d17855caf1d28df04399a8f250336a0c

fixed checking text position relative to viewport (again)
This change adds support for font draw offsets when checking if a text
output is inside the main (playfield) viewport.

This change corrects commit 06aebcbc, which broke the preview label on
the main screen of the level set "Snake Bite".

src/tools.c
@@ -3607,8 +3607,8 @@ static boolean inGfxField_FontHeight(struct TextPosInfo *pos)
   if (pos->x == -1 && pos->y == -1)	// marked as outside viewport
     return FALSE;
 
-  int x = SX + pos->x;
-  int y = SY + pos->y + getFontHeight(pos->font);
+  int x = SX + pos->x + getFontDrawOffsetX(pos->font);
+  int y = SY + pos->y + getFontDrawOffsetY(pos->font) + getFontHeight(pos->font);
 
   if (!IN_GFX_FIELD_FULL(x, y))
     return FALSE;